320677a9df18d70ddfec6479854d6241e4daa022
SabrinaHikariYamauti <hikarijpfriends@hotmail.com>
2021-09-15 10:38:59 -0300

    feat(app):added initial files for app

---
+++ bird-app/src/app/pages/main/main.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bird-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage{
+
+  constructor() { }
+
+}
